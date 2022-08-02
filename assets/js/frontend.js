1e77526483e323fccf91580f8714cdaec2ede1e5
Mar <99101383+qipeot@users.noreply.github.com>
2022-08-01 21:50:00 -0300

    add index, css and js

---
+++ assets/js/frontend.js
@@ -0,0 +1,73 @@
+$('.atendimento-carousel').slick({    
+    nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
+    autoplay: false,
+    autoplaySpeed: 2500,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            }
+        }]
+  });
+
+  $('.estatistica-carousel').slick({    
+    nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
+    autoplay: false,
+    autoplaySpeed: 2500,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            }
+        }]
+  });
+
+  $('.noticias-carousel').slick({    
+    nextArrow: '<i class="fas fa-chevron-right slick-next"></i>',
+    prevArrow: '<i class="fas fa-chevron-left slick-prev"></i>',
+    autoplay: false,
+    autoplaySpeed: 2500,
+    dots: false,
+    arrows: true,
+    infinite: true,
+    speed: 500,
+    cssEase: 'linear',
+    slidesToShow: 4,
+    slidesToScroll: 3,
+    responsive: [{
+        breakpoint: 991,
+        settings: {
+            dots: false,
+            arrows: true,
+            slidesToShow: 1,
+            slidesToScroll: 1,
+            autoplay: true,
+            }
+        }]
+  });
+
+
